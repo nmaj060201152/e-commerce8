de86d6bb8ec4055f8699df2bda0f7e51c9cf3bfd
nmaj060201152 <https://github.com/nmaj060201152>
2025-01-04 04:06:56 +0500

    e-commerce8

---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+export default function Post() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div>
+      <h1>Post {id}</h1>
+      <p>This is the content for post {id}.</p>
+    </div>
+  );
+}
